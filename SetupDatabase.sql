a2d6f5c07dfaaeb7fc04f6eebcb1b6875b08c012
Skyler Bexten <skyler.bexten@gmail.com>
2023-04-29 11:08:20 -0600

    Create material and category table

--- SetupDatabase.sql
+++ SetupDatabase.sql
@@ -1,2 +1,17 @@
-CREATE DATABASE Inventory;
+CREATE DATABASE Inventory
+GO
+
+USE [Inventory];
+GO
+
+CREATE TABLE [Material](
+	[Material_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Material_Name] [nchar](25) NULL
+);
+GO
+
+CREATE TABLE [Category](
+	[Category_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Category_Name] [nchar](25) NULL
+);
 GO
