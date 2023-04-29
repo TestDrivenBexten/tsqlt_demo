cdc22f561ac3f76d0f2d5a32d70d07d640c4b0b2
Skyler Bexten <skyler.bexten@gmail.com>
2023-04-29 12:37:23 -0600

    Add two more deletion tests

--- sql/test_cases/spDeleteLowQualityItemTest.sql
+++ sql/test_cases/spDeleteLowQualityItemTest.sql
@@ -20,3 +20,43 @@ AS BEGIN
     EXEC tSQLt.assertEquals 1, @rowCount;
 END;
 GO
+
+CREATE PROCEDURE spDeleteLowQualityItemTest.[test should delete below threshold]
+AS BEGIN
+    -- Arrange
+    DECLARE @rowCount INT;
+    EXEC tSQLt.FakeTable 'Item';
+    INSERT INTO Item (Item_ID, Material_ID, Item_Quality)
+    VALUES (10, 20, 30);
+
+    -- Act
+    EXEC spDeleteLowQualityItem 40, 20;
+
+    -- Assert
+    SELECT
+        @rowCount = COUNT(*)
+    FROM
+        Item;
+    EXEC tSQLt.assertEquals 0, @rowCount;
+END;
+GO
+
+CREATE PROCEDURE spDeleteLowQualityItemTest.[test should not delete item of different material]
+AS BEGIN
+    -- Arrange
+    DECLARE @rowCount INT;
+    EXEC tSQLt.FakeTable 'Item';
+    INSERT INTO Item (Item_ID, Material_ID, Item_Quality)
+    VALUES (10, 123, 30);
+
+    -- Act
+    EXEC spDeleteLowQualityItem 40, 20;
+
+    -- Assert
+    SELECT
+        @rowCount = COUNT(*)
+    FROM
+        Item;
+    EXEC tSQLt.assertEquals 1, @rowCount;
+END;
+GO
