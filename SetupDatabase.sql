7341f30e1a118cedeedd2bed8530065996bb87e9
Skyler Bexten <skyler.bexten@gmail.com>
2023-04-29 11:19:30 -0600

    Create join on material and category

--- SetupDatabase.sql
+++ SetupDatabase.sql
@@ -5,13 +5,21 @@ USE [Inventory];
 GO
 
 CREATE TABLE [Material](
-	[Material_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Material_ID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Material_Name] [nchar](25) NULL
 );
 GO
 
 CREATE TABLE [Category](
-	[Category_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Category_ID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Category_Name] [nchar](25) NULL
+);
+GO
+
+CREATE TABLE [MaterialCategory](
+	[Category_ID] INT NOT NULL,
+	[Material_ID] INT NOT NULL,
+    CONSTRAINT FK_Category FOREIGN KEY (Category_ID) REFERENCES Category (Category_ID),
+    CONSTRAINT FK_Material FOREIGN KEY (Material_ID) REFERENCES Material (Material_ID)
 );
 GO
