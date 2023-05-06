6add80c0e8c9d60983edb754867d6e083dc2c5f1
Skyler Bexten <skyler.bexten@gmail.com>
2023-05-06 17:23:52 -0600

    Execute sql query for tSQLt test procedure

--- configure-db.ps1
+++ configure-db.ps1
@@ -85,4 +85,4 @@ ExecuteSqlFile $inventorySqlParameters "sql/test_cases/spCraftRecipeTest.sql"
 ExecuteSqlFile $inventorySqlParameters "sql/test_cases/spDeleteLowQualityItemTest.sql"
 
 # Run test cases
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d Inventory -Q "EXEC tSQLt.RunAll;"
+ExecuteSqlQuery $inventorySqlParameters "EXEC tSQLt.RunAll"
